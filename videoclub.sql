151fb2d895dc2f6dcd51528afd4cffdcbb771b24
Hector <a29546@svalero.com>
2026-04-25 01:25:11 +0200

    Añadido script SQL de estructura limpio

--- videoclub.sql
+++ videoclub.sql
@@ -1,28 +1,8 @@
-/*M!999999\- enable the sandbox mode */ 
--- MariaDB dump 10.19-11.7.2-MariaDB, for Win64 (AMD64)
---
--- Host: localhost    Database: videoclub
--- ------------------------------------------------------
--- Server version	12.1.2-MariaDB
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
-
---
--- Table structure for table `alquiler`
---
+CREATE DATABASE videoclub;
+USE videoclub;
 
 DROP TABLE IF EXISTS `alquiler`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `alquiler` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_pelicula` int(11) NOT NULL,
@@ -40,23 +20,17 @@ CREATE TABLE `alquiler` (
   CONSTRAINT `2` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`),
   CONSTRAINT `3` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `alquiler`
---
 
 LOCK TABLES `alquiler` WRITE;
-/*!40000 ALTER TABLE `alquiler` DISABLE KEYS */;
+
 INSERT INTO `alquiler` VALUES
 (7,1,2,4,'2026-04-08','2026-04-24','devuelto',2.99),
 (8,2,2,5,'2026-04-24','2026-05-02','activo',2.99);
 /*!40000 ALTER TABLE `alquiler` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `cliente`
---
+
 
 DROP TABLE IF EXISTS `cliente`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -153,17 +127,8 @@ INSERT INTO `pelicula` VALUES
 /*!40000 ALTER TABLE `pelicula` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Dumping routines for database 'videoclub'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+SHOW TABLES;
+SELECT * FROM pelicula;
+SELECT * FROM cliente;
+SELECT * FROM empleado;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
-
--- Dump completed on 2026-04-25  0:58:24
